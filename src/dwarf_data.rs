e890b976f6e58b8c9485b8279f2056bcdd6e8735
XiaYuan Wu <i2256g03c3@163.com>
2023-12-18 13:49:20 +0800

    add dwarf_data

--- src/dwarf_data.rs
+++ src/dwarf_data.rs
@@ -14,3 +14,15 @@ pub struct DwarfData {
     files: Vec<File>,
     add2line: Context<addr2line::gimli::EndianRcSlice<addr2line::gimli::RunTimeEndian>>,
 }
+
+impl fmt::Debug for DwarfData {
+    fn fmt(&self, f:&mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "")
+    }
+}
+
+
+impl DwarfData {
+    pub fn from_file(path: &str}) -> Result<DwarfData, Error> {
+    }
+}

ea926ca176b36e623c09d488c64a469c6bd3632e
XiaYuan Wu <i2256g03c3@163.com>
2023-12-19 13:34:20 +0800

    add dwarf_data

--- src/dwarf_data.rs
+++ src/dwarf_data.rs
@@ -17,12 +17,25 @@ pub struct DwarfData {
 
 impl fmt::Debug for DwarfData {
     fn fmt(&self, f:&mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "")
+            write!(f, "DwarfData {{files: {:?}}}", self.files)
     }
 }
 
+impl From<gimli_wrapper::Error> for Error {
+    fn from(err: gimli_wrapper::Error) -> Self {
+        Error::DwarfFormatError(err)
+    }
+}
 
 impl DwarfData {
     pub fn from_file(path: &str}) -> Result<DwarfData, Error> {
+        let file = fs::File::open(path).or(Err(Error::ErrorOpeningFile))?;
+        let mmap = unsafe { 
+            memmap::Mmap::map(&files).or(Err(Error::ErrorOpeningFile))?
+        };
+        let object = object::File::parse(&*mmap)
+            .or_else(|e| Err(gimli_wrapper::Error::ObjectError(e.to_string())))?;
+        let endian = if object.is_little_endian() {
+        }
     }
 }
